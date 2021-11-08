cedac1c447fc8ceed9df4b12ca9608c15256e59b
Vahagn Nalbandyan <vaagnnalbandyan@gmail.com>
2021-11-08 21:22:53 +0400

    Revenue Comparison Graph Started

--- src/components/revenue/Revenue.js
+++ src/components/revenue/Revenue.js
@@ -1,13 +1,21 @@
 import React, { useRef } from "react";  
 
 import '../../styles/components/revenue.css';
+import GraphWithCompareTo from "./revenue-components/GraphWithCompareTo";
 
 const Revenue = () => {
     const revenueSection = useRef();    
 
     return(
         <div className='revenue-body' id='revenue-body-analytics'>
-            <div ref={revenueSection} className='revenue-wrapper'>Revenue</div>
+            <div ref={revenueSection} className='revenue-wrapper'>
+                <div className='revenue-section-1'>
+                    <div className='revenue-compare-graph-cont'>
+                        <p>Graph With Compare To</p>
+                        <GraphWithCompareTo />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
